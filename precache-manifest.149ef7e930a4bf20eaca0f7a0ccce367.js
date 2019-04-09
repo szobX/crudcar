50eeda31c4368d24f40a1d78962395d729d04e8d
Patryk <szoberpatryk@op.pl>
2019-04-09 17:56:27 +0200

    Updates

---
+++ precache-manifest.149ef7e930a4bf20eaca0f7a0ccce367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8acaff4c26cf04328b42",
+    "url": "szobx.github.io/crudcar/static/css/main.7e817095.chunk.css"
+  },
+  {
+    "revision": "8acaff4c26cf04328b42",
+    "url": "szobx.github.io/crudcar/static/js/main.1b967d1e.chunk.js"
+  },
+  {
+    "revision": "21c9a5ea3f6602866382",
+    "url": "szobx.github.io/crudcar/static/js/runtime~main.cd5480f6.js"
+  },
+  {
+    "revision": "bef635a7775b93b69598",
+    "url": "szobx.github.io/crudcar/static/css/2.fc1689cb.chunk.css"
+  },
+  {
+    "revision": "bef635a7775b93b69598",
+    "url": "szobx.github.io/crudcar/static/js/2.7aad8eef.chunk.js"
+  },
+  {
+    "revision": "393e2879300c2b8af9c97cb8d8d722bb",
+    "url": "szobx.github.io/crudcar/index.html"
+  }
+];
